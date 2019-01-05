92c5c264471b229fe39ae31023c29be1b4d66aa8
Ahmed Fasih <wuzzyview@gmail.com>
2019-01-04 20:39:27 -0500

    Include kanji in output (better for AWS Polly)

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import * as kamiya from 'kamiya-codec';
+let kanji = `書く 泳ぐ 話す 待つ 死ぬ 飛ぶ 読む 取る 買う 見る 食べる 来る する`.split(' ');
+let kana = `かく およぐ はなす まつ しぬ とぶ よむ とる かう みる たべる くる する`.split(' ');
+if (kana.length !== kanji.length) { throw new Error('bad lengths'); }
 
-let type1Verbs = 'かく およぐ はなす まつ しぬ とぶ よむ とる かう'.split(' ');
-let type2Verbs = 'みる たべる'.split(' ');
-let irregularVerbs = 'くる する'.split(' ');
 let conj = [
   kamiya.Conjugation.Dictionary,
   kamiya.Conjugation.Negative,
@@ -13,37 +13,42 @@ let conj = [
   kamiya.Conjugation.Volitional,
 ];
 
-let engBase = 'write,swim,speak,wait,die,fly,read,take,buy,see,eat,come,do'.split(',');
-let engConj = `X
-(I) don't X
-(I) X (polite)
-(I) X'ed
-if (I) X
-X!
-(I) will X/let’s X`.split('\n');
-let header = 'Dict,negative,polite,past,conditional,imperative,volitional'.split(',');
-if (header.length !== conj.length) { throw new Error('header.length!==conj.length?'); }
-
 let endings = ',ない,ます,,ば,,う'.split(',');
 if (endings.length !== conj.length) { throw new Error('endings.length!==conj.length?'); }
 const append = (left: string, right: string) => left + (left.endsWith(right) ? '' : right);
 const verbToRow = (v: string, type2: boolean) =>
     conj.map((c, i) => append(kamiya.conjugate(v, c, type2)[0], endings[i]));
 
-let type1 = type1Verbs.map(v => verbToRow(v, false));
-let type2 = type2Verbs.map(v => verbToRow(v, true));
-let irreg = irregularVerbs.map(v => verbToRow(v, false));
+let [type1kanji, type1kana] = [kanji, kana].map(vs => vs.slice(0, 8).map(v => verbToRow(v, false)));
+let [type2kanji, type2kana] = [kanji, kana].map(vs => vs.slice(8, 10).map(v => verbToRow(v, true)));
+let [irregkanji, irregkana] = [kanji, kana].map(vs => vs.slice(10).map(v => verbToRow(v, false)));
+let tableKanji = type1kanji.concat(type2kanji).concat(irregkanji);
+let tableKana = type1kana.concat(type2kana).concat(irregkana);
+
+let engBase = 'write,swim,speak,wait,die,fly,read,take,buy,see,eat,come,do'.split(',');
+let engConj = `X
+(I) don’t X
+(I) X (polite)
+(I) X’ed
+if (I) X
+X!
+(I) will X/let’s X`.split('\n');
+let conjNames = 'Dict,negative,polite,past,conditional,imperative,volitional'.split(',');
+if (conjNames.length !== conj.length) { throw new Error('header.length!==conj.length?'); }
 
 export function* enumerate<T>(v: T[]|IterableIterator<T>, n: number = 0): IterableIterator<[number, T]> {
   for (let x of v) { yield [n++, x]; }
 }
 
-let table = type1.concat(type2).concat(irreg);
-let allVerbs = type1Verbs.concat(type2Verbs).concat(irregularVerbs);
-console.log('base\tconjugated\tEnglish');
+console.log('base\tbase kana\tconjugated\tconjugated kana\tEnglish');
 for (let cidx = 1; cidx < conj.length; cidx++) {
-  for (let [vi, v] of enumerate(allVerbs)) {
-    console.log(`${v} (${header[cidx]})\t${table[vi][cidx]}\t${engConj[cidx].replace(/X/g, engBase[vi])}`);
+  for (let [vi, v] of enumerate(kanji)) {
+    let conj = tableKanji[vi][cidx];
+    let conjKana = tableKana[vi][cidx];
+    let eng = engConj[cidx].replace(/X/g, engBase[vi]);
+    let base = `${v} (${conjNames[cidx]})`;
+    let baseKana = `${kana[vi]} (${conjNames[cidx]})`;
+    console.log([base, baseKana, conj, conjKana, eng].join('\t'));
   }
   console.log('\n')
 }
